2364afa65ef1d3f60403af940f719dde2c57a729
Marcin Włodarczyk <gonzur75@gmail.com>
2024-06-27 12:26:35 +0200

    beeter version of app with DRY, rusability, hashbang and much more

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
-with open('text.txt', 'r') as file:
-    for line in file.readlines():
-        for word in line.split():
-            print(word)
+#!/usr/bin/env python3
+
+import sys
+
+
+def fetch_words(filepath):
+    with open(filepath , 'r') as file:
+        return [word for line in file for word in line.split()]
+
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+def main(filepath):
+    words = fetch_words(filepath)
+    print_items(words)
+
+
+# fetch_words('text.txt')
+if __name__ == '__main__':
+    main(sys.argv[1])
